04412fdb1c6d383719acdbb6829120882087e924
Stefan Hadzhiev <stefan.hadzhiev@sap.com>
2022-06-16 16:19:15 +0300

    Add dark theme functionlaity + adjust some styles to make it work

--- src/components/IconButton/IconButton.jsx
+++ src/components/IconButton/IconButton.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import styles from './styles.module.scss';
 
-export const IconButton = ({ icon, tooltip, onClick }) => {
+export const IconButton = ({ icon, tooltip, onClick, darkTheme }) => {
     return (
-        <button className={styles['button']} onClick={onClick}>
+        <button
+            className={`${styles['button']} ${
+                darkTheme
+                    ? styles['button-dark-theme']
+                    : styles['button-light-theme']
+            }`}
+            onClick={onClick}
+        >
             {icon}
         </button>
     );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,42 +4,86 @@ import { AiFillHome, AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 import { MdFileDownload } from 'react-icons/md';
 import { IconButton } from 'components';
 import { FaFacebookF } from 'react-icons/fa';
+import { BsFillSunFill } from 'react-icons/bs';
+import { GiMoon } from 'react-icons/gi';
 
-export const Navbar = () => {
+export const Navbar = ({ darkTheme, changeTheme }) => {
     const buttons = [
         {
             id: 'home',
-            icon: <AiFillHome />,
+            icon: (
+                <AiFillHome
+                    className={darkTheme ? 'svg-dark-theme' : ''}
+                />
+            ),
             tooltip: 'Home',
         },
         {
             id: 'github',
-            icon: <AiFillGithub />,
+            icon: (
+                <AiFillGithub
+                    className={darkTheme ? 'svg-dark-theme' : ''}
+                />
+            ),
             tooltip: 'Github',
         },
         {
             id: 'linkedIn',
-            icon: <AiFillLinkedin />,
+            icon: (
+                <AiFillLinkedin
+                    className={darkTheme ? 'svg-dark-theme' : ''}
+                />
+            ),
             tooltip: 'LinkedIn',
         },
         {
             id: 'resume',
-            icon: <MdFileDownload />,
+            icon: (
+                <MdFileDownload
+                    className={darkTheme ? 'svg-dark-theme' : ''}
+                />
+            ),
             tooltip: 'My resume',
         },
         {
             id: 'facebook',
-            icon: <FaFacebookF />,
+            icon: (
+                <FaFacebookF
+                    className={darkTheme ? 'svg-dark-theme' : ''}
+                />
+            ),
             tooltip: 'Facebook',
+        },
+        {
+            id: 'theme',
+            icon: darkTheme ? (
+                <GiMoon className={darkTheme ? 'svg-dark-theme' : ''} />
+            ) : (
+                <BsFillSunFill
+                    className={darkTheme ? 'svg-dark-theme' : ''}
+                />
+            ),
+            tooltip: darkTheme ? 'Light theme' : 'Dark theme',
+            onClick: changeTheme,
         },
     ];
 
     const renderButtons = buttons.map(btn => (
-        <IconButton icon={btn.icon} key={btn.id} tooltip={btn.tooltip} />
+        <IconButton
+            darkTheme={darkTheme}
+            icon={btn.icon}
+            key={btn.id}
+            tooltip={btn.tooltip}
+            onClick={btn.onClick}
+        />
     ));
 
     return (
-        <div className={styles['navbar']}>
+        <div
+            className={`${styles['navbar']} ${
+                darkTheme ? 'dark-theme' : 'light-theme'
+            }`}
+        >
             <div className={styles['navbar__buttons']}>
                 {renderButtons}
             </div>
